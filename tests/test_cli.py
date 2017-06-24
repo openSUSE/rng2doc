2d6f14f1eee238156a48d101fbab787a481bcbc0
Thomas Schraitle <toms@suse.de>
2017-06-24 20:46:07 +0200

    Add testcases for cli.py

---
+++ tests/test_cli.py
@@ -0,0 +1,37 @@
+import pytest
+from rng2doc.cli import parsecli, main
+
+
+@pytest.mark.parametrize('cli,expected', [
+  (['-o', 'out.xml', 'in.rng'],
+   {'RNGFILE': 'in.rng',
+    '--output': 'out.xml'}
+   ),
+  (['-v', '-o', 'out.xml', 'in.rng'],
+   {'-v': 1,
+    'RNGFILE': 'in.rng',
+    '--output': 'out.xml'}
+   ),
+  (['-vv', '-o', 'out.xml', 'in.rng'],
+   {'-v': 2,
+    'RNGFILE': 'in.rng',
+    '--output': 'out.xml'}
+   ),
+  (['-vvv', '-o', 'out.xml', 'in.rng',],
+   {'-v': 3,
+    'RNGFILE': 'in.rng',
+    '--output': 'out.xml'}
+   ),
+])
+def test_parsecli(cli, expected):
+    result = parsecli(cli)
+    # Create set difference and only compare this with the expected dictionary
+    assert {item: result.get(item, None) for item in expected} == expected
+
+
+#@pytest.mark.parametrize('cli,expected', [
+#    (['-o', 'out.xml', 'in.rng'],
+#     ),
+#])
+#def test_main(cli, expected):
+#    result = main()
