0650cef5cea664cf3692913075179401ea65f59f
Thomas Schraitle <toms@suse.de>
2017-06-25 11:47:55 +0200

    Improve testcase for rng2doc.cli.checkargs()

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,11 @@
+# Standard Library
+from unittest.mock import patch
 
 # Third Party Libraries
 import pytest
 
 # My Stuff
-from rng2doc.cli import main, parsecli
+from rng2doc.cli import checkargs, main, parsecli
 
 
 @pytest.mark.parametrize('cli,expected', [
@@ -31,3 +33,20 @@ def test_parsecli(cli, expected):
     result = parsecli(cli)
     # Create set difference and only compare this with the expected dictionary
     assert {item: result.get(item, None) for item in expected} == expected
+
+
+@patch('rng2doc.cli.os.path.exists')
+def test_checkargs_found_rng(mock_exists):
+    mock_exists.return_value = True
+    assert checkargs({'RNGFILE': 'fake.rng'}) is None
+
+@patch('rng2doc.cli.os.path.exists')
+def test_checkargs_notfound_rng(mock_exists):
+    mock_exists.return_value = False
+    with pytest.raises(FileNotFoundError):
+        checkargs({'RNGFILE': 'fake.rng'})
+
+def test_checkargs_with_DocoptExit():
+    from docopt import DocoptExit
+    with pytest.raises(DocoptExit):
+        checkargs({'RNGFILE': None})
