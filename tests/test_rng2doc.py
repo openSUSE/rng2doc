bc996aefd97b8a614b798ce2ee09688b2456b152
Thomas Schraitle <toms@suse.de>
2017-06-25 12:06:35 +0200

    Add more tests for rng2doc.cli.main()

--- tests/test_rng2doc.py
+++ tests/test_rng2doc.py
@@ -1,5 +1,7 @@
 # Standard Library
+import logging
 import os
+from unittest.mock import patch
 
 # Third Party Libraries
 import pytest
@@ -7,6 +9,8 @@ import pytest
 # My Stuff
 import rng2doc
 from rng2doc.cli import main
+
+# log = logging.getLogger(rng2doc.__package__)
 
 
 def test_main():
@@ -16,17 +20,31 @@ def test_main():
 def test_main_with_version(capsys):
     """Checks for correct version"""
     with pytest.raises(SystemExit):
-        main(["", "--version"])
+        main([rng2doc.__package__, "--version"])
     out, _ = capsys.readouterr()
     assert out == "{} {}\n".format(rng2doc.__package__, rng2doc.__version__)
 
 
-def test_main_with_capsys(capsys):
+def test_main_with_capsys():
     """Checks, if __main__.py can be executed"""
     with pytest.raises(SystemExit):
         path = os.path.dirname(os.path.realpath(__file__)) +\
                "/../src/{}/__main__.py".format(rng2doc.__package__)
         exec(compile(open(path).read(), path, "exec"), {}, {"__name__": "__main__"})
+
+
+@patch('rng2doc.cli.os.path.exists')
+def test_main_notfound_rng(mock_exists):
+    mock_exists.return_value = False
+    result = main([rng2doc.__package__, "fake.rng"])
+    assert result != 0
+
+
+@patch('rng2doc.cli.os.path.exists')
+def test_main_with_KeyboardInterrupt(mock_exists):
+    mock_exists.side_effect = KeyboardInterrupt()
+    result = main([rng2doc.__package__, "fake.rng"])
+    assert result == 10
 
 
 #@pytest.mark.parametrize('cli,expected', [
