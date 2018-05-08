52d2bd453373c65fa2b2ba8ad28d4bedd057999f
Jürgen Löhel <jloehel@suse.com>
2018-05-08 18:16:19 +0200

    Adds some tests for the new flag.
    Signed-off-by: Jürgen Löhel <jloehel@suse.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,31 +3,41 @@ from unittest.mock import patch
 
 # Third Party Libraries
 import pytest
+from lxml import etree
 
 # My Stuff
 from rng2doc.cli import checkargs, main, parsecli
 
 
 @pytest.mark.parametrize('cli,expected', [
-  (['-o', 'out.xml', 'in.rng'],
-   {'RNGFILE': 'in.rng',
-    '--output': 'out.xml'}
-   ),
-  (['-v', '-o', 'out.xml', 'in.rng'],
-   {'-v': 1,
-    'RNGFILE': 'in.rng',
-    '--output': 'out.xml'}
-   ),
-  (['-vv', '-o', 'out.xml', 'in.rng'],
-   {'-v': 2,
-    'RNGFILE': 'in.rng',
-    '--output': 'out.xml'}
-   ),
-  (['-vvv', '-o', 'out.xml', 'in.rng',],
-   {'-v': 3,
-    'RNGFILE': 'in.rng',
-    '--output': 'out.xml'}
-   ),
+    (['-o', 'out.xml', 'in.rng'],
+     {'RNGFILE': 'in.rng',
+      '--output-format': 'xml',
+      '--output': 'out.xml'}
+    ),
+    (['-v', '-o', 'out.xml', 'in.rng'],
+     {'-v': 1,
+      'RNGFILE': 'in.rng',
+      '--output-format': 'xml',
+      '--output': 'out.xml'}
+    ),
+    (['-vv', '-o', 'out.xml', 'in.rng'],
+     {'-v': 2,
+      'RNGFILE': 'in.rng',
+      '--output-format': 'xml',
+      '--output': 'out.xml'}
+    ),
+    (['-vvv', '-o', 'out.xml', 'in.rng',],
+     {'-v': 3,
+      'RNGFILE': 'in.rng',
+      '--output-format': 'xml',
+      '--output': 'out.xml'}
+    ),
+    (['--output-format', 'html', '-o', 'out.xml', 'in.rng',],
+     {'RNGFILE': 'in.rng',
+      '--output-format': 'html',
+      '--output': 'out.xml'}
+    ),
 ])
 def test_parsecli(cli, expected):
     result = parsecli(cli)
@@ -38,16 +48,24 @@ def test_parsecli(cli, expected):
 @patch('rng2doc.cli.os.path.exists')
 def test_checkargs_found_rng(mock_exists):
     mock_exists.return_value = True
-    assert checkargs({'RNGFILE': 'fake.rng'}) is None
+    assert checkargs({'RNGFILE': 'fake.rng', '--output-format': 'xml'}) is None
 
 
 @patch('rng2doc.cli.os.path.exists')
 def test_checkargs_notfound_rng(mock_exists):
     mock_exists.return_value = False
     with pytest.raises(FileNotFoundError):
-        checkargs({'RNGFILE': 'fake.rng'})
+        checkargs({'RNGFILE': 'fake.rng', '--output-format': 'xml'})
+
+
+@patch('rng2doc.cli.os.path.exists')
+def test_checkargs_unknown_output_format(mock_exists):
+    mock_exists.return_value = True
+    with pytest.raises(RuntimeError):
+        checkargs({'RNGFILE': 'fake.rng', '--output-format': 'test'})
+
 
 def test_checkargs_with_DocoptExit():
     from docopt import DocoptExit
     with pytest.raises(DocoptExit):
-        checkargs({'RNGFILE': None})
+        checkargs({'RNGFILE': None, '--output-format': 'xml'})
--- tests/test_rng2doc.py
+++ tests/test_rng2doc.py
@@ -6,6 +6,7 @@ from unittest.mock import patch
 # Third Party Libraries
 import pytest
 from docopt import DocoptExit
+from lxml import etree
 
 # My Stuff
 import rng2doc
@@ -59,33 +60,11 @@ def test_main_with_KeyboardInterrupt(mock_parsecli):
     assert result == errorcode(KeyboardInterrupt())
 
 
-# @patch('rng2doc.cli.checkargs')
-# @patch('rng2doc.cli.parsecli')
-# @patch('rng2doc.cli.process')
-# def test_main_with_success_of_process(mock_process, mock_parsecli, mock_checkargs):
-#     mock_process.return_value = 1000
-#     mock_parsecli.return_value = {'RNGFILE': 'fake.rng',
-#                                   '--version': False,
-#                                   '-v': 0,
-#                                   '--output': None}
-#     mock_checkargs.return_value = None
-# 
-#     result = main([rng2doc.__package__, "fake.rng"])
-#     assert result == 1000
-# 
-@pytest.mark.skip
-@patch('rng2doc.cli.checkargs')
-def test_main_with_FileNotFound(mock_checkargs):
-    mock_checkargs.side_effect = FileNotFoundError()
-    print(">>>>")
-    result = main([rng2doc.__package__, "fake.rng"])
-    assert mock_checkargs.called
-    assert result == errorcode(FileNotFoundError())
-
-
-#@pytest.mark.parametrize('cli,expected', [
-#    (['-o', 'out.xml', 'in.rng'],
-#     ),
-#])
-#def test_main(cli, expected):
-#    result = main()
+@patch('rng2doc.cli.os.path.exists')
+@patch('rng2doc.rng.etree.parse')
+def test_checkargs_empty_rng(mock_exists, mock_parse):
+    def xmltree(source, parser=None, base_url=None):
+        return etree.XML("""""").getroottree()
+    mock_exists.return_value = True
+    mock_parse.side_effect = xmltree
+    assert main(['fake.rng']) == 20
