4104300a952edfdb9ad88b2409ddcf173e2b7447
Jürgen Löhel <juergen@loehel.de>
2021-07-21 13:08:06 -0500

    Adds global
    Signed-off-by: Jürgen Löhel <juergen@loehel.de>

--- src/rng2doc/log.py
+++ src/rng2doc/log.py
@@ -9,6 +9,8 @@ logger = logging.getLogger("rng2doc")
 
 
 def setup_logging(level):
+    global logger
+
     logger.setLevel(level)
     handler = logging.StreamHandler()
     handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
